ef042f2124a25768e826a52bc93d296116a2591f
梅津吉雅 <umetsu.yoshinori.uv4@is.naist.jp>
2022-05-05 23:23:36 +0900

    [add]データベースの作成とダミーデータの追加

---
+++ database/seeders/TweetsSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+
+class TweetsSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('tweets')->insert([
+            'content' => Str::random(100),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
