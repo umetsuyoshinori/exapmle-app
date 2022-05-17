035060036d199e7ad90164d1044cb3cb4cabdedb
梅津吉雅 <umetsu.yoshinori.uv4@is.naist.jp>
2022-05-17 23:05:39 +0900

    [add]CRUDのすべての処理を実装、コンポーネント化

--- resources/views/components/tweet/list.blade.php
+++ resources/views/components/tweet/list.blade.php
@@ -12,7 +12,8 @@
                     <p class="text-gray-600">{!! nl2br(e($tweet->content)) !!}</p>
                 </div>
                 <div>
-                    <!-- TODO 編集と削除 -->
+                    <x-tweet.options :tweetId="$tweet->id" :userId="$tweet->user_id">
+                    </x-tweet.options>
                 </div>
             </li>
         @endforeach
